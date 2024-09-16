245d086d4c282bdac5738e6baea0d41ddc440f1c
m1mmseen <m1mmseen@proton.me>
2024-09-16 11:34:18 +0200

    Authorization Card optimized

--- src/Authorization.jsx
+++ src/Authorization.jsx
@@ -3,16 +3,17 @@ import Button from "./components/helper/Button.jsx";
 // eslint-disable-next-line react/prop-types
 export default function Authorization({token, setToken, handleFetchData}) {
     return (
-        <div className="w-2/3 mx-auto shadow-xl rounded-xl border-2 border-rvblue-100 p-[5rem]">
+        <div className="mx-auto my-auto shadow-xl rounded-xl border-2 border-rvblue-100 p-[5rem]">
             <h2 className="text-rvblue-900 py-4 text-xl">AuthThoken eingeben</h2>
-            <div className="flex gap-4">
+            <div className="flex flex-col gap-4">
                     <textarea
-                        className="h-[15rem] w-[20rem] rounded-md border-2 border-rvblue-200 focus:outline-none focus:border-b-2 focus:border-rvblue-200 text-center"
-                        placeholder="Gib dein Token ein"
+                        className="h-[15rem] w-[50rem] rounded-md border-2 border-rvblue-200 focus:outline-none focus:border-b-2 focus:border-rvblue-200 text-center"
+                        placeholder="Gib deinen Token ein"
                         value={token}
                         onChange={(e) => setToken(e.target.value)}
                     />
                 <Button
+                    width="w-[25rem]"
                     action={handleFetchData}>Daten abrufen
                 </Button>
             </div>
--- src/components/helper/Button.jsx
+++ src/components/helper/Button.jsx
@@ -1,7 +1,7 @@
-export default function Button({children, action}) {
+export default function Button({children, action, width}) {
     return <button
         onClick={action}
-        className="bg-rvblue-400 text-rvblue-900 h-[3rem] px-4 rounded-xl hover:bg-rvblue-200 hover:text-rvblue-800">
+        className={`bg-rvblue-400 text-rvblue-100 text-lg h-[3rem] px-4 rounded-xl hover:bg-rvblue-200 hover:text-rvblue-800 ${width}`}>
         {children}
     </button>;
 }
